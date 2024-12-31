dbd30dccf37e36a5e7f73c1738fd2e9b7217749b
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-12-31 14:16:01 +0900

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -2,9 +2,9 @@ const observer = new MutationObserver(() => {
     const adBadge = document.querySelector('.ytp-ad-badge__text--clean-player');
     if (adBadge) {
         console.log('広告をスキップします');
-        const videoElement = document.querySelector('.html5-main-video');
+        const videoElement = document.querySelector('video');
         if (videoElement) {
-            videoElement.currentTime += 999; 
+            videoElement.currentTime = 999; 
         }
     }
 });
